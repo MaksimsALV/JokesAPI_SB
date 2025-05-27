6f3e6b4a6f0e470051b27f130a5868514567f52a
MaksimsALV <maxim.ahmetov@gmail.com>
2025-05-27 18:16:29 +0300

    adding field; connecting API; fixing index.html

--- JokesAPI_SB/src/main/java/com/jokesapi/JokesAPI_SB/webcontroller/WebController.java
+++ JokesAPI_SB/src/main/java/com/jokesapi/JokesAPI_SB/webcontroller/WebController.java
@@ -1,13 +1,27 @@
 package com.jokesapi.JokesAPI_SB.webcontroller;
 
+import org.springframework.ui.Model;
+import com.jokesapi.JokesAPI_SB.API.Joke;
+import com.jokesapi.JokesAPI_SB.API.JokesAPI;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @Controller
 public class WebController {
 
     @GetMapping("/")
-    public String index() {
-        return "index"; // Maps to index.html in templates folder
+    public String index(Model model) {
+        Joke joke = null;
+        try {
+            joke = JokesAPI.requestAPI();
+        } catch (Exception e) {
+            //todo need to add error logging somehow here
+        }
+        model.addAttribute("joke", joke);
+        return "index";
     }
 }
